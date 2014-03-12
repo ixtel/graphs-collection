adaf0cdf89dd086d450497a826d61db478893674
Matthew Henderson <matthew.james.henderson@gmail.com>
2014-03-12 17:33:02 +0000

    Now test data parameters are parsed from a JSON object.

--- tests/test.c
+++ tests/test.c
@@ -1,34 +1,52 @@
 #include <assert.h>
 #include <stdio.h>
 #include <igraph.h>
+#include <jansson.h>
 
 int main(int argc, char *argv[])
 {
   igraph_t g;
-  igraph_real_t radius;
-  igraph_integer_t diameter;
-  igraph_integer_t girth;
+  igraph_real_t _radius;
+  igraph_integer_t _diameter;
+  igraph_integer_t _girth;
   FILE *ifile;
+  json_t *json;
+  json_t *vcount, *ecount, *girth, *diameter, *radius;
+  json_error_t error;
+
   
   ifile = fopen("../Classic/Chvatal/chvatal.gml", "r");
   if (ifile == 0) {
     return 10;
   }
-  
+
   igraph_read_graph_gml(&g, ifile);
+
+  json = json_load_file("../Classic/Chvatal/chvatal_properties.json", 0, &error);
+  if(!json) {
+       /* the error variable contains error information */
+  }
+
   fclose(ifile);
 
-  assert(igraph_vcount(&g) == 12);
-  assert(igraph_ecount(&g) == 24);
+  // Get data from JSON object.
+  vcount = json_object_get(json, "n_vertices");
+  ecount = json_object_get(json, "n_edges");
+  radius = json_object_get(json, "radius");
+  diameter = json_object_get(json, "diameter");
+  girth = json_object_get(json, "girth");
 
-  igraph_radius(&g, &radius, IGRAPH_ALL);
-  assert(radius == 2);
+  // Use igraph to compute graph parameters.
+  igraph_radius(&g, &_radius, IGRAPH_ALL);
+  igraph_diameter(&g, &_diameter, 0, 0, 0, IGRAPH_UNDIRECTED, 1);
+  igraph_girth(&g, &_girth, 0);
 
-  igraph_diameter(&g, &diameter, 0, 0, 0, IGRAPH_UNDIRECTED, 1);
-  assert(diameter == 2);
-
-  igraph_girth(&g, &girth, 0);
-  assert(girth == 4);
+  // Test all computed parametes match data.
+  assert(igraph_vcount(&g) == json_integer_value(vcount));
+  assert(igraph_ecount(&g) == json_integer_value(ecount));
+  assert(_radius == json_integer_value(radius));
+  assert(_diameter == json_integer_value(diameter));
+  assert(_girth == json_integer_value(girth));
 
   igraph_destroy(&g);
   
