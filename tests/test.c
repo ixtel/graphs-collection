e14fd03454b7eb39b56753df9be6b4b6512a2c8c
Matthew Henderson <matthew.james.henderson@gmail.com>
2014-03-17 20:11:49 +0000

    Now using CUnit (http://cunit.sourceforge.net) for testing.

--- tests/test.c
+++ tests/test.c
@@ -2,71 +2,97 @@
 #include <stdio.h>
 #include <igraph.h>
 #include <jansson.h>
+#include "CUnit/Basic.h"
 
-// Given a graph and a properties JSON object, check that all
-// of the properties are satisfied by the graph.
-int test(igraph_t *graph, json_t *data)
+static FILE* i_file = NULL;
+static json_t *json;
+static igraph_t g;
+
+int init_suite1(void)
 {
+  json_error_t error;
+  if (NULL == (i_file = fopen("../src/Classic/Chvatal/chvatal.gml", "r")) ||
+     (NULL == (json = json_load_file("../src/Classic/Chvatal/chvatal_properties.json", 0, &error)))) {
+     return -1;
+  }
+  else {
+     return 0;
+  }
+
+}
+
+int clean_suite1(void)
+{
+  if (0 != fclose(i_file)) {
+    return -1;
+  }
+  else {
+     i_file = NULL;
+     return 0;
+  }
+}
+
+void test_chvatal(void)
+{
+  igraph_read_graph_gml(&g, i_file);
+
   json_t *vcount, *ecount, *girth, *diameter, *radius;
   igraph_real_t _radius;
   igraph_integer_t _diameter;
   igraph_integer_t _girth;
 
   // Get data from JSON object.
-  vcount = json_object_get(data, "n_vertices");
-  ecount = json_object_get(data, "n_edges");
-  radius = json_object_get(data, "radius");
-  diameter = json_object_get(data, "diameter");
-  girth = json_object_get(data, "girth");
+  vcount = json_object_get(json, "n_vertices");
+  ecount = json_object_get(json, "n_edges");
+  radius = json_object_get(json, "radius");
+  diameter = json_object_get(json, "diameter");
+  girth = json_object_get(json, "girth");
 
   // Use igraph to compute graph parameters.
-  igraph_radius(graph, &_radius, IGRAPH_ALL);
-  igraph_diameter(graph, &_diameter, 0, 0, 0, IGRAPH_UNDIRECTED, 1);
-  igraph_girth(graph, &_girth, 0);
+  igraph_radius(&g, &_radius, IGRAPH_ALL);
+  igraph_diameter(&g, &_diameter, 0, 0, 0, IGRAPH_UNDIRECTED, 1);
+  igraph_girth(&g, &_girth, 0);
 
   // Test all computed parameters match data.
-  assert(igraph_vcount(graph) == json_integer_value(vcount));
-  assert(igraph_ecount(graph) == json_integer_value(ecount));
+  assert(igraph_vcount(&g) == json_integer_value(vcount));
+  assert(igraph_ecount(&g) == json_integer_value(ecount));
   assert(_radius == json_integer_value(radius));
   assert(_diameter == json_integer_value(diameter));
   assert(_girth == json_integer_value(girth));
 
-  return 0;
+  igraph_destroy(&g);
 }
 
-// Given a file containing a graph (in GML format) and a JSON
-// properties object, test that all properties hold for the graph
-// contained in the file.
-int check_properties(FILE *graph_file, json_t *properties) {
-  igraph_t g;
-  igraph_read_graph_gml(&g, graph_file);
-  test(&g, properties);
-  igraph_destroy(&g);
+void test_tutte(void)
+{
 }
 
 int main(int argc, char *argv[])
 {
-  FILE *ifile;
-  json_t *json;
-  json_error_t error;
+  CU_pSuite pSuite = NULL;
 
-  // Open a graph file in GML format.
-  ifile = fopen("../src/Classic/Chvatal/chvatal.gml", "r");
-  if (ifile == 0) {
-    return 10;
+  /* initialize the CUnit test registry */
+  if (CUE_SUCCESS != CU_initialize_registry())
+     return CU_get_error();
+
+  /* add a suite to the registry */
+  pSuite = CU_add_suite("Suite_1", init_suite1, clean_suite1);
+  if (NULL == pSuite) {
+     CU_cleanup_registry();
+     return CU_get_error();
   }
 
-  // Load a JSON file.
-  json = json_load_file("../src/Classic/Chvatal/chvatal_properties.json", 0, &error);
-  if(!json) {
-       /* the error variable contains error information */
+  /* add the tests to the suite */
+  if ((NULL == CU_add_test(pSuite, "Test of Chvatal graph", test_chvatal)) ||
+      (NULL == CU_add_test(pSuite, "Test of Tutte graph", test_tutte)))
+  {
+     CU_cleanup_registry();
+     return CU_get_error();
   }
 
-  // Check graph properties.
-  check_properties(ifile, json);
-
-  // Cleanup.
-  fclose(ifile);
-
-  return 0;
+  /* Run all tests using the CUnit Basic interface */
+  CU_basic_set_mode(CU_BRM_VERBOSE);
+  CU_basic_run_tests();
+  CU_cleanup_registry();
+  return CU_get_error();
 }
