4f4c083391a0768e01da2323db57f37071dcc1c0
Matthew Henderson <matthew.james.henderson@gmail.com>
2014-04-10 16:59:31 +0100

    Building of all Platonic graphs.

--- rules.mk
+++ rules.mk
@@ -1,6 +1,13 @@
 %.gexf:
 	$(pangraph) --type gexf $(OPTIONS) -o $@
 
+%.gml:
+	$(pangraph) --type gml $(OPTIONS) -o $@
+	sed -i '/label/d' $@
+
+%.graphml:
+	$(pangraph) --type graphml $(OPTIONS) -o $@
+
 %.net:
 	$(pangraph) --type pajek $(OPTIONS) -o $@
 
--- src/Platonic/Tetrahedral/tetrahedral.gml
+++ src/Platonic/Tetrahedral/tetrahedral.gml
@@ -2,19 +2,15 @@ graph [
   name "Platonic Tetrahedral graph"
   node [
     id 0
-    label 0
   ]
   node [
     id 1
-    label 1
   ]
   node [
     id 2
-    label 2
   ]
   node [
     id 3
-    label 3
   ]
   edge [
     source 0
