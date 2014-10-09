4f82cd76b844c4bc3093b9a1a03bc1a91f41669e
Matthew Henderson <matthew.james.henderson@gmail.com>
2014-10-09 20:30:53 +0100

    Implementations of size and order for graph6 testing.

--- src/graphs.bash
+++ src/graphs.bash
@@ -15,3 +15,7 @@ gv_maxdeg() { cat $1 | gvpr -fmaxdeg | sed -n 's/max degree = \([0-9]*\).*/\1/p'
 g6_girth() { cat $1 | girth.py; }
 
 g6_diameter() { cat $1 | diameter.py; }
+
+g6_order() { cat $1 | listg -y | gc -n | awk '{ print $1 }'; }
+
+g6_size() { cat $1 | listg -y | gc -e | awk '{ print $1 }'; }
