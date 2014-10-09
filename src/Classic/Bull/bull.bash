5d4e97789f31e75851a2f730acf53da9bd3882fc
Matthew Henderson <matthew.james.henderson@gmail.com>
2014-10-09 14:36:47 +0100

    Now testing values are computed by functions.

--- src/Classic/Bull/bull.bash
+++ src/Classic/Bull/bull.bash
@@ -2,18 +2,12 @@ bull_dir=$HOME/workspace/graphs/graphs-collection/src/Classic/Bull
 bull_gv_path=$bull_dir/bull.gv
 bull_properties_path=$bull_dir/bull_properties.yml
 
-get_property() {
-  property=$1
-  s="/$property:/"'{ print $2  }'
-  echo `cat $bull_properties_path | awk -F": " "$s"`
-}
+bull_expected_chi=$(get_property $bull_properties_path chromatic-number)
+bull_expected_order=$(get_property $bull_properties_path order)
+bull_expected_size=$(get_property $bull_properties_path size)
+bull_expected_maxdeg=$(get_property $bull_properties_path max-degree)
 
-bull_expected_chi=$(get_property chromatic-number)
-bull_expected_order=$(get_property order)
-bull_expected_size=$(get_property size)
-bull_expected_maxdeg=$(get_property max-degree)
-
-bull_gv_computed_chi=`chromatic $bull_gv_path`
-bull_gv_computed_order=`gc -n $bull_gv_path | awk '{ print $1 }'`
-bull_gv_computed_size=`gc -e $bull_gv_path | awk '{ print $1 }'`
-bull_gv_computed_maxdeg=`cat $bull_gv_path | gvpr -fmaxdeg | sed -n 's/max degree = \([0-9]*\).*/\1/p'`
+bull_gv_computed_chi=$(gv_chromatic $bull_gv_path)
+bull_gv_computed_order=$(gv_order $bull_gv_path)
+bull_gv_computed_size=$(gv_size $bull_gv_path)
+bull_gv_computed_maxdeg=$(gv_maxdeg $bull_gv_path)
--- src/Classic/Chvatal/chvatal.bash
+++ src/Classic/Chvatal/chvatal.bash
@@ -2,18 +2,12 @@ chvatal_dir=$HOME/workspace/graphs/graphs-collection/src/Classic/Chvatal
 chvatal_gv_path=$chvatal_dir/chvatal.gv
 chvatal_properties_path=$chvatal_dir/chvatal_properties.yml
 
-get_property() {
-  property=$1
-  s="/$property:/"'{ print $2  }'
-  echo `cat $chvatal_properties_path | awk -F": " "$s"`
-}
+chvatal_expected_chi=$(get_property $chvatal_properties_path chromatic-number)
+chvatal_expected_order=$(get_property $chvatal_properties_path order)
+chvatal_expected_size=$(get_property $chvatal_properties_path size)
+chvatal_expected_maxdeg=$(get_property $chvatal_properties_path max-degree)
 
-chvatal_expected_chi=$(get_property chromatic-number)
-chvatal_expected_order=$(get_property order)
-chvatal_expected_size=$(get_property size)
-chvatal_expected_maxdeg=$(get_property max-degree)
-
-chvatal_gv_computed_chi=`chromatic $chvatal_gv_path`
-chvatal_gv_computed_order=`gc -n $chvatal_gv_path | awk '{ print $1 }'`
-chvatal_gv_computed_size=`gc -e $chvatal_gv_path | awk '{ print $1 }'`
-chvatal_gv_computed_maxdeg=`cat $chvatal_gv_path | gvpr -fmaxdeg | sed -n 's/max degree = \([0-9]*\).*/\1/p'`
+chvatal_gv_computed_chi=$(gv_chromatic $chvatal_gv_path)
+chvatal_gv_computed_order=$(gv_order $chvatal_gv_path)
+chvatal_gv_computed_size=$(gv_size $chvatal_gv_path)
+chvatal_gv_computed_maxdeg=$(gv_maxdeg $chvatal_gv_path)
