0ad05a7e280b05e661c1554cb2aab75999ecb400
Matthew Henderson <matthew.james.henderson@gmail.com>
2014-10-09 10:51:08 +0100

    Testing of the Bull graph from YAML property data.

--- tests/chromatic.sh
+++ tests/chromatic.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bats
 
+setup() {
+  gv_chi_computer=chromatic
+  src_dir=$HOME/workspace/graphs/graphs-collection/src
+}
+
 @test "Bull graph." {
-  result="$(chromatic ../src/Classic/Bull/bull.gv)"
-  [ "$result" -eq 3 ]
+  bull_dir="$src_dir/Classic/Bull"
+  bull_gv_path="$bull_dir/bull.gv"
+  bull_properties_path="$bull_dir/bull_properties.yml"
+  bull_expected_chi=`cat $bull_properties_path | awk -F": " '/chromatic-number:/ { print $2  }'`
+  bull_gv_computed_chi="$($gv_chi_computer $bull_gv_path)"
+  [ "$bull_gv_computed_chi" -eq $bull_expected_chi ]
 }
 
 @test "Chvatal graph." {
